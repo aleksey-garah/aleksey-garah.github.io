eb337c68b8f0d596732859ad7744609fed01648c
AlexG <liohaharakh@gmail.com>
2017-02-10 20:40:20 +0300

    comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
 
+    var scroll = 0;
+    $(window).scroll(function (event) {
+        scroll = $(window).scrollTop();
+        if (scroll > 0) {
+            $('.top-navigation').addClass("start-scroll");
+        } else {
+            $('.top-navigation').removeClass("start-scroll");
+        }
+    });
+
     $("#section-main-prices-table-tabs").tabs();
     $("#catalog-info-tabs").tabs();
     $("#project-card-info-tabs").tabs();
---
+++ pages/about-company/comments/comments.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    if ($(window).width() > 1024) {
+        $("#main-materials-images-block-tabs").tabs({
+            active: 1
+        });
+    } else {
+        $("#main-materials-images-block-tabs").tabs({
+            active: 0
+        });
+    }
+});
