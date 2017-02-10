a3bda3437ffc840e608f5e8f218018ca6759cde8
AlexG <liohaharakh@gmail.com>
2017-02-10 17:15:22 +0300

    added half responsive for catalog

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
 $(document).ready(function () {
 
-    var scroll = 0;
-    $(window).scroll(function (event) {
-        scroll = $(window).scrollTop();
-        if (scroll > 0) {
-            $('.top-navigation').addClass("start-scroll");
-        } else {
-            $('.top-navigation').removeClass("start-scroll");
-        }
-    });
-
     $("#section-main-prices-table-tabs").tabs();
     $("#catalog-info-tabs").tabs();
     $("#project-card-info-tabs").tabs();
@@ -21,23 +11,6 @@ $(document).ready(function () {
     }
 
     $("#homes-subsection-galery-tabs").tabs();
-
-    $("#slider-range").slider({
-        min: 0,
-        max: 1000,
-        values: [0, 1000],
-        range: true,
-        stop: function (event, ui) {
-            $("input#minCost").val($("#slider-range").slider("values", 0));
-            $("input#maxCost").val($("#slider-range").slider("values", 1));
-        },
-
-        slide: function (event, ui) {
-            $("input#minCost").val($("#slider-range").slider("values", 0));
-            $("input#maxCost").val($("#slider-range").slider("values", 1));
-        }
-    });
-
 });
 
 $(window).on("load", function () {
---
+++ pages/projects/catalog.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    var scroll = 0;
+    $(window).scroll(function (event) {
+        scroll = $(window).scrollTop();
+        if (scroll > 0) {
+            $('.top-navigation').addClass("start-scroll");
+        } else {
+            $('.top-navigation').removeClass("start-scroll");
+        }
+    });
+
+    $("#catalog-info-tabs").tabs();
+});
