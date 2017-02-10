a3d4619bef6908ff64d7dcb51aa227799a1267f6
AlexG <liohaharakh@gmail.com>
2017-02-10 18:00:39 +0300

    added responsive for catalog

--- pages/projects/catalog.js
+++ pages/projects/catalog.js
@@ -11,4 +11,12 @@ $(document).ready(function () {
     });
 
     $("#catalog-info-tabs").tabs();
+
+    $(".catalog-info-tabs-button").on("click", function (event) {
+        $("#catalog-info-tabs").toggleClass("active")
+    });
+
+    $(".left-filters-block-button").on("click", function (event) {
+        $(".left-filters-block").toggleClass("active")
+    })
 });
