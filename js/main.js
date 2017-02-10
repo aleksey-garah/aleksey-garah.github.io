e0e18f9709ef43b8ba6db7f3db984f9e261a5ce7
AlexG <liohaharakh@gmail.com>
2017-02-10 06:41:37 +0300

    added something

--- js/main.js
+++ js/main.js
@@ -14,5 +14,37 @@ $(document).ready(function () {
     $("#catalog-info-tabs").tabs();
     $("#project-card-info-tabs").tabs();
 
+    if ($(window).width() > 1024) {
+        $("#main-materials-images-block-tabs").tabs({
+            active: 1
+        });
+    }
 
+    $("#homes-subsection-galery-tabs").tabs();
+
+    $("#slider-range").slider({
+        min: 0,
+        max: 1000,
+        values: [0, 1000],
+        range: true,
+        stop: function (event, ui) {
+            $("input#minCost").val($("#slider-range").slider("values", 0));
+            $("input#maxCost").val($("#slider-range").slider("values", 1));
+        },
+
+        slide: function (event, ui) {
+            $("input#minCost").val($("#slider-range").slider("values", 0));
+            $("input#maxCost").val($("#slider-range").slider("values", 1));
+        }
+    });
+
+});
+
+$(window).on("load", function () {
+    // $(".team-members-list-container").mCustomScrollbar({
+    //     axis: "x",
+    //     scrollButtons: {
+    //         enable: true
+    //     }
+    // });
 });
