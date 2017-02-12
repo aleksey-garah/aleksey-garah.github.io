6a0245323ae0437648d9459b0f5ebb68cd4dd3ea
AlexG <liohaharakh@gmail.com>
2017-02-12 21:10:21 +0300

    changed colors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $( "#calculate-dialog" ).dialog({
+    $("#calculate-dialog").dialog({
         autoOpen: false
     });
 
@@ -8,7 +8,7 @@ $(document).ready(function () {
         $("#calculate-dialog").dialog("open");
     });
 
-    $(".ui-dialog ").on("click", function (event) {
+    $(".ui-dialog").on("click", function (event) {
         $("#calculate-dialog").dialog("close");
     });
 
@@ -24,4 +24,13 @@ $(window).on("load", function () {
             enable: true
         }
     });
+
+    $(".team-members-list-item").on("click", function (event) {
+        $(".team-members-list-item").removeClass("active");
+        $(this).addClass("active");
+
+        $(".team-member-block-image").children().attr("src", $(this).children("img").attr("src"));
+        $(".team-member-block-fio").text($(this).children(".team-members-list-item-fio").text());
+        $(".team-member-block-title").text($(this).children(".team-members-list-item-title").text());
+    })
 });
