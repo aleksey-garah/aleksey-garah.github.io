81f453142d60e25f8dd55739c2a471b0f60700fd
AlexG <liohaharakh@gmail.com>
2017-02-12 16:38:23 +0300

    added fonts finished with navigation

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
     });
 
     $("#section-main-prices-table-tabs").tabs();
-    $("#catalog-info-tabs").tabs();
     $("#project-card-info-tabs").tabs();
 });
 
@@ -22,4 +21,60 @@ $(window).on("load", function () {
             enable: true
         }
     });
+});
+
+/*--------------------------------------------------
+ Hidden Nav
+ --------------------------------------------------*/
+hiddenNavBar = {
+    $menu: $('#menu'),
+    init: function () {
+        this.resize();
+        $('<div id="on-hidden-menu"><div class="toggle "><span></span></div><ul></ul></div>').hide().insertAfter(".nav-phone-block");
+        // toggle
+        $('#on-hidden-menu .toggle').click(function () {
+            $('#on-hidden-menu').toggleClass('open');
+        });
+
+        // win load & resize
+        $(window).on('load resize', function () {
+            hiddenNavBar.resize();
+        });
+    },
+    resize: function () {
+        setTimeout(function () {
+            var menuWidth = $('#menu').width();
+            var winW = $(".main-navigation-menu").width();
+
+            console.log(menuWidth, winW);
+
+            if (winW > menuWidth) {
+                console.log('init');
+
+                $('#on-hidden-menu').show();
+                $clone = $('li:not(".on-hidden"):last', $('#menu')).addClass('on-hidden').clone();
+
+                if ($clone.parent().length == 0) {
+                    $clone.prependTo($('#on-hidden-menu ul'));
+                }
+
+                hiddenNavBar.resize();
+
+            } else if (winW + $('li.on-hidden:first').width() < menuWidth) {
+                $('li.on-hidden:first').removeClass('on-hidden');
+                $('#on-hidden-menu ul li:first').remove();
+            }
+
+            if ($('.on-hidden').length == 0) {
+                $('#on-hidden-menu').removeClass('open').hide();
+            }
+        }, 10);
+    }
+};
+
+/*--------------------------------------------------
+ DOC READY
+ --------------------------------------------------*/
+$(function () {
+    hiddenNavBar.init();
 });
