f98282f90c4e2c7935f7a0f429aacbb843f1e97c
AlexG <liohaharakh@gmail.com>
2017-02-14 01:19:26 +0300

    fixes

--- pages/projects/catalog.js
+++ pages/projects/catalog.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
     $("#catalog-info-tabs").tabs();
 
     $(".catalog-info-tabs-button").on("click", function (event) {
-        $("#catalog-info-tabs").toggleClass("active")
+        $("#catalog-info-tabs").toggleClass("active");
+        $(".left-filters-block").removeClass("active");
     });
 
     $(".left-filters-block-button").on("click", function (event) {
-        $(".left-filters-block").toggleClass("active")
+        $(".left-filters-block").toggleClass("active");
+        $("#catalog-info-tabs").removeClass("active");
     });
 
     $("#order-call-dialog").dialog({
