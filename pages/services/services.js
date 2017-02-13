75ac47de9d949e236b6b1b789f16692e537dc811
AlexG <liohaharakh@gmail.com>
2017-02-13 04:40:20 +0300

    services

---
+++ pages/services/services.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $("#order-call-dialog").dialog({
+        autoOpen: false
+    });
+    $("#open-order-call-window").on("click", function (event) {
+        $("#order-call-dialog").dialog("open");
+    });
+
+    //===========================================
+
+    $(".ui-dialog").on("click", function (event) {
+        $("#order-call-dialog").dialog("close");
+    });
+
+    $(".ui-dialog-content").on("click", function (event) {
+        event.stopPropagation();
+    });
+});
