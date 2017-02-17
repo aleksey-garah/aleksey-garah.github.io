ac685e0a095b399fda54026dd1cb7ba0ae462490
AlexG <liohaharakh@gmail.com>
2017-02-17 20:52:12 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -13,57 +13,52 @@ $(document).ready(function () {
     $("#opne-mobile-menu-button").on("click", function (even) {
         $("#mobile-menu").toggleClass("active");
     });
-});
-
-$(window).on("load", function () {
-    hiddenNavBar = {
-        $menu: $('#menu'),
-        init: function () {
-            this.resize();
-            $('<div id="on-hidden-menu"><div class="toggle "><span></span></div><ul></ul></div>').hide().insertAfter(".nav-phone-block");
-            // toggle
-            $('#on-hidden-menu .toggle').click(function () {
-                $('#on-hidden-menu').toggleClass('open');
-            });
-
-            // win load & resize
-            $(window).on('load resize', function () {
-                hiddenNavBar.resize();
-            });
-        },
-        resize: function () {
-            setTimeout(function () {
-                var menuWidth = $('#menu').width();
-                var winW = $(".main-navigation-menu").width();
-
-                console.log(menuWidth, winW);
-
-                if (winW > menuWidth) {
-                    console.log('init');
-
-                    $('#on-hidden-menu').show();
-                    $clone = $('li:not(".on-hidden"):last', $('#menu')).addClass('on-hidden').clone();
-
-                    if ($clone.parent().length == 0) {
-                        $clone.prependTo($('#on-hidden-menu ul'));
-                    }
-
-                    hiddenNavBar.resize();
-
-                } else if (winW + $('li.on-hidden:first').width() < menuWidth) {
-                    $('li.on-hidden:first').removeClass('on-hidden');
-                    $('#on-hidden-menu ul li:first').remove();
-                }
-
-                if ($('.on-hidden').length == 0) {
-                    $('#on-hidden-menu').removeClass('open').hide();
-                }
-            }, 10);
-        }
-    };
 
     hiddenNavBar.init();
-
-    var height = $(window).height()/2 - $(".modal-window ").height()/2;
-    $(".ui-dialog-titlebar").css("top", height);
 });
+
+hiddenNavBar = {
+    $menu: $('#menu'),
+    init: function () {
+        this.resize();
+        $('<div id="on-hidden-menu"><div class="toggle "><span></span></div><ul></ul></div>').hide().insertAfter(".nav-phone-block");
+        // toggle
+        $('#on-hidden-menu .toggle').click(function () {
+            $('#on-hidden-menu').toggleClass('open');
+        });
+
+        // win load & resize
+        $(window).on('load resize', function () {
+            hiddenNavBar.resize();
+        });
+    },
+    resize: function () {
+        setTimeout(function () {
+            var menuWidth = $('#menu').width();
+            var winW = $(".main-navigation-menu").width();
+
+            console.log(menuWidth, winW);
+
+            if (winW > menuWidth) {
+                console.log('init');
+
+                $('#on-hidden-menu').show();
+                $clone = $('li:not(".on-hidden"):last', $('#menu')).addClass('on-hidden').clone();
+
+                if ($clone.parent().length == 0) {
+                    $clone.prependTo($('#on-hidden-menu ul'));
+                }
+
+                hiddenNavBar.resize();
+
+            } else if (winW + $('li.on-hidden:first').width() < menuWidth) {
+                $('li.on-hidden:first').removeClass('on-hidden');
+                $('#on-hidden-menu ul li:first').remove();
+            }
+
+            if ($('.on-hidden').length == 0) {
+                $('#on-hidden-menu').removeClass('open').hide();
+            }
+        }, 10);
+    }
+};
